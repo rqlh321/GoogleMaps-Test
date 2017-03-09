f4bdbe1a123c39f49bcf70d7de224c9e086f32b5
develop25 <develop25@dongis.ru>
2017-03-09 11:18:47 +0300

    test

--- app/src/main/java/com/example/sic/googlemapstesting/MapsActivity.java
+++ app/src/main/java/com/example/sic/googlemapstesting/MapsActivity.java
@@ -12,11 +12,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.PolygonOptions;
 import com.google.android.gms.maps.model.PolylineOptions;
 
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
@@ -36,6 +41,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onMapReady(final GoogleMap googleMap) {
+timer(googleMap);
+    }
+
+    private void timer(final GoogleMap googleMap) {
+        Consumer<LatLng> consumer = new Consumer<LatLng>() {
+            @Override
+            public void accept(LatLng latLng) throws Exception {
+                MarkerOptions options = new MarkerOptions();
+                options.position(latLng);
+                googleMap.addMarker(options);
+            }
+        };
+
+        Observable<LatLng> observable = Observable.timer(500, TimeUnit.MILLISECONDS)
+                .map(new Function<Long, LatLng>() {
+                    @Override
+                    public LatLng apply(Long aLong) throws Exception {
+                        return new LatLng(Math.random() * 100, Math.random() * 0);
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        Disposable subscription = observable.subscribe(consumer);
+
+     //   subscription.dispose();
+
+    }
+
+    private void interval(final GoogleMap googleMap) {
         final ArrayList<LatLng> list = new ArrayList<>();
         list.add(new LatLng(0, 0));
         final PolygonOptions polygonOptions = new PolygonOptions();
@@ -89,32 +124,36 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .observeOn(AndroidSchedulers.mainThread());
 
         observable.subscribe(lines);
-        // observable.subscribe(points);
-        //   observable.subscribe(polygon);
+        observable.subscribe(points);
+        observable.subscribe(polygon);
 
 
     }
 
-    private void timer(final GoogleMap googleMap) {
-        Consumer<? super LatLng> subscriber = new Consumer<LatLng>() {
+    private void range() {
+        Flowable.range(1, 3).subscribe(new Subscriber<Integer>() {
+
             @Override
-            public void accept(LatLng latLng) throws Exception {
-                MarkerOptions options = new MarkerOptions();
-                options.position(latLng);
-                googleMap.addMarker(options);
+            public void onSubscribe(Subscription s) {
+                System.out.println("OnSubscribe start");
+                s.request(1);
+                System.out.println("OnSubscribe end");
             }
-        };
 
-        Observable.timer(500, TimeUnit.MILLISECONDS)
-                .map(new Function<Long, LatLng>() {
-                    @Override
-                    public LatLng apply(Long aLong) throws Exception {
-                        return new LatLng(Math.random() * 10, Math.random() * 10);
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(subscriber);
+            @Override
+            public void onNext(Integer v) {
+                System.out.println(v);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("Done");
+            }
+        });
     }
-
 }
